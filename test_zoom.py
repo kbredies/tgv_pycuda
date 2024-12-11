29a9a805830562e94f0dbeab7adc08addb5357d4
Kristian Bredies <kristian.bredies@uni-graz.at>
2024-12-11 16:27:11 +0100

    Added main readme file.

--- test_zoom.py
+++ test_zoom.py
@@ -103,13 +103,13 @@ g = u.get()
 imwrite("results/" + base + "_small.png", g)  # , vmin=0.0, vmax=1.0)
 
 print("\nTV zooming\n----------")
-alphas = linspace(0.1, 0.1, 1)
+alphas = linspace(0.05, 0.15, 11)
 u_tv, alpha_tv, psnr_tv, time_tv = test_range(power, g, f, alphas)
 print(f"TV best parameter: alpha={alpha_tv}, PSNR={psnr_tv}, time={time_tv}")
 imwrite("results/" + base + "_zoomed_tv.png", u_tv)
 
 print("\nTGV zooming\n-----------")
-alphas = linspace(0.1, 0.1, 1)
+alphas = linspace(0.01, 0.11, 11)
 u_tgv, alpha_tgv, psnr_tgv, time_tgv = test_range(
     power, g, f, alphas, tgv=True)
 print(
